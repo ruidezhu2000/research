f3c0d4eaba54f1deb9e2ee729ac7be3452fba9ad
ruidezhu2000 <90271374+ruidezhu2000@users.noreply.github.com>
2023-02-09 22:01:58 -0600

    Update week1_task.py

--- week1_task.py
+++ week1_task.py
@@ -3,51 +3,66 @@ import collections
 import csv
 
 # PART 1
-itr = pd.read_stata('uspatentcitation.dta', iterator = True)
+itr = pd.read_stata('uspatentcitation.dta', iterator=True)
+# itr = pd.read_stata('sample data.csv', iterator=True)
 # data = itr.get_chunk(5)
 index = 0
-patents = collections.defaultdict(list)
+patents = collections.defaultdict(list)  # {citation_id : [patent_id,category]}
 issuedDateTable = {}
 for curr in itr:
-    if curr["category"].values.item(0) == "": 
+    if curr["category"].values.item(0) == "":
         continue
 
-    # get issued_date 
-    if curr["patent_id"].values.item(0) not in issuedDateTable:
-        issuedDateTable[curr["patent_id"].values.item(0)] = curr["date"].values.item(0)
+    # get issued_date based on citation_id
+    if curr["citation_id"].values.item(0) not in issuedDateTable:
+        issuedDateTable[curr["citation_id"].values.item(
+            0)] = curr["date"].values.item(0)
 
-    info = [int(curr["date"].values.item(0)[:4]),curr["category"].values.item(0)]
-    patents[curr["patent_id"].values.item(0)].append(info)
-    
+    info = [curr["patent_id"].values.item(0), curr["category"].values.item(0)]
+    patents[curr["citation_id"].values.item(0)].append(info)
+
     # stopper
     index += 1
-    if index == 50000: break
+    if index == 50000:
+        break
+
 # print(patents)
 
-# fake data for testing 
-# patents = {'10456544': [[1943, 'cited by applicant'],[1943, 'cited by applicant'],[1944, 'cited by applicant']], '8250307': [[2008, 'cited by other']], 'D490798': [[1961, 'cited by examiner']], '9199394': [[1993, 'cited by applicant']], '10796471': [[2010, 'cited by applicant']]}
 
 with open('table1.csv', 'w', newline='') as file:
     writer = csv.writer(file)
-    writer.writerow(['patent_id', 'issued_date', '# of citations made_all', '# of citations made_applicant', 'year', '# of citations received'])
-    for patent,data in patents.items():
-        count = {}
-        
+    writer.writerow(['patent_id', 'issued_date', '# of citations made_all',
+                    '# of citations made_applicant', 'year', '# of citations received'])
+    for patent, data in patents.items():
+        count = {}  # {year XXXX : [applicantCount, totalThisYear]}
+
         for d in data:
-            # print("d:",d)
-            # print(d.)
-            if d[0] not in count: 
-                count[d[0]] = [0,0] #applicantCount, totalThisYear
-            temp = count[d[0]]
+            if d[0] not in issuedDateTable:
+                continue
+            year_xxxx = int(issuedDateTable[d[0]][:4])
+            if year_xxxx not in count:
+                # applicantCount, totalThisYear
+                count[year_xxxx] = [0, 0]
+            temp = count[year_xxxx]
             # print(temp)
             if d[1] == 'cited by applicant':
                 temp[0] += 1
             temp[1] += 1
-            count[d[0]] = temp
+            count[year_xxxx] = temp
+
         yearOrder = sorted(count.items())
-        for year in yearOrder:
-            # print("year:",year[0])
-            writer.writerow([patent,issuedDateTable[patent], len(data),year[1][0], year[0], year[1][1]])
+        if not yearOrder:
+            continue
+        start = yearOrder[0][0]
+        end = yearOrder[-1][0]
+
+        for year in range(start, end):
+            if year in count:
+                writer.writerow([patent, issuedDateTable[patent], len(
+                    data), count[year][0], year, count[year][1]])
+            else:
+                writer.writerow(
+                    [patent, issuedDateTable[patent], len(data), 0, year, 0])
 
 
 # PART 2
@@ -59,7 +74,7 @@ with open('table1.csv', 'w', newline='') as file:
 # for curr in itr:
 #     # print(curr)
 #     # print("RZ",len(curr),curr['patnum'])
-    
+
 #     # print(type(curr),type(curr["patnum"]),type(curr["patnum"].values.item(0)))
 #     companyInfo = [curr["patnum"].values.item(0),curr["fdate"].values.item(0),curr["idate"].values.item(0),curr["year"].values.item(0)]
 #     companies[curr["permco"].values.item(0)].append(companyInfo)
@@ -68,6 +83,3 @@ with open('table1.csv', 'w', newline='') as file:
 #     if index == 5: break
 
 # print(companies)
-
-
-    
